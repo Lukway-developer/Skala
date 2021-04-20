83f36cbaf5bf5138a69b5ffca6c437b7a891d4f7
Lucas Ojeda de Sousa <lukway.developer@gmail.com>
2021-04-20 13:35:50 -0300

    Add 'category' and 'onfire'

--- index.js
+++ index.js
@@ -9,163 +9,217 @@ const data = {
       title: 'Lama Negra',
       description: 'Suavidad y brillo para cabellos oscuros y sin vida',
       price: '700',
-      image: './images/cremas/Lama Negra.jpg'
+      image: './images/cremas/Lama Negra.jpg',
+      category: 'expert',
+      onfire: 'true'
     }, {
       id: 1,
       title: 'Mas Lisos',
       description: 'AntiFrizz para cabellos naturalmente lisos o alisados',
       price: '700',
-      image: './images/cremas/Mais Lisos.jpg'
+      image: './images/cremas/Mais Lisos.jpg',
+      category: 'expert',
+      onfire: 'true'
     }, {
       id: 2,
       title: 'Doña Skala',
       description: 'Hidratación y brillo para cabellos lisos u ondulados',
       price: '700',
-      image: './images/cremas/Dona Skala.jpg'
+      image: './images/cremas/Dona Skala.jpg',
+      category: 'expert',
+      onfire: false
     }, {
       id: 3,
       title: 'Babosa',
       description: 'Hidratación profunda para cabellos rizados',
       price: '700',
-      image: './images/cremas/Babosa.jpg'
+      image: './images/cremas/Babosa.jpg',
+      category: 'expert',
+      onfire: false
     }, {
       id: 4,
       title: 'Jaborandi',
       description: 'Hidratación y sorprendete fuerza para cabellos débiles',
       price: '700',
-      image: './images/cremas/Jaborandi.jpg'
+      image: './images/cremas/Jaborandi.jpg',
+      category: 'expert',
+      onfire: false
     }, {
       id: 5,
       title: 'Poción Divina',
       description: 'Hidratación, brillo y antifrizz. Aceites, keratina y colágeno',
+      price: '700',
       image: './images/cremas/Divino Potão.jpg',
-      price: '700'
+      category: 'potão',
+      onfire: false
     }, {
       id: 6,
       title: 'Poción Divina Kids',
       description: 'Acción desenredante para cabellos ondulados y rizados',
       price: '700',
-      image: './images/cremas/Divino Potinho.jpg'
+      image: './images/cremas/Divino Potinho.jpg',
+      category: 'potão',
+      onfire: false
     }, {
       id: 7,
       title: 'Poción Dormida',
       description: 'Cabellos hidratados, aliñados y sin frizz',
+      price: '700',
       image: './images/cremas/Potão Desmaiado.jpg',
-      price: '700'
+      category: 'potão',
+      onfire: false
     }, {
       id: 8,
       title: 'Poción de Amor',
       description: 'Revitalización y renovación para cabellos dañados',
       price: '700',
-      image: './images/cremas/Potão do Amor.jpg'
+      image: './images/cremas/Potão do Amor.jpg',
+      category: 'potão',
+      onfire: false
     }, {
       id: 9,
-      title: 'Almidón de Maiz',
+      title: 'Almidón de Maíz',
       description: 'Ultra reparación, fuerza y brillo para todo tipo de cabellos',
       price: '700',
-      image: './images/cremas/Amido de Milho.jpg'
+      image: './images/cremas/Amido de Milho.jpg',
+      category: 'expert',
+      onfire: false
     }, {
       id: 10,
       title: 'Spa Naturals',
       description: 'Activación de crecimiento y puntas hidratadas',
       price: '700',
-      image: './images/cremas/Spa Naturals.jpg'
+      image: './images/cremas/Spa Naturals.jpg',
+      category: 'spa naturals',
+      onfire: false
     }, {
       id: 11,
       title: 'Bomba de Vitaminas',
       description: 'Bomba de vitaminas para todo tipo de cabellos',
       price: '700',
-      image: './images/cremas/Bomba de Vitaminas.jpg'
+      image: './images/cremas/Bomba de Vitaminas.jpg',
+      category: 'especials',
+      onfire: false
     }, {
       id: 12,
       title: 'Brillo Increíble',
       description: 'Brillo increíble para todo tipo de cabellos',
       price: '700',
-      image: './images/cremas/Brilho Incrivel.jpg'
+      image: './images/cremas/Brilho Incrivel.jpg',
+      category: 'especials',
+      onfire: false
     }, {
       id: 13,
       title: 'Restauración Total',
       description: 'Restauración espectacular para todo tipo de cabellos',
       price: '700',
-      image: './images/cremas/Restauração Espectacular.jpg'
+      image: './images/cremas/Restauração Espectacular.jpg',
+      category: 'especials',
+      onfire: false
     }, {
       id: 14,
       title: 'Fuerza Extrema',
       description: 'Hidratación y fuerza extrema para cabellos rizados',
       price: '700',
-      image: './images/cremas/Força Extrema.jpg'
+      image: './images/cremas/Força Extrema.jpg',
+      category: 'especials',
+      onfire: false
     }, {
       id: 15,
       title: 'Fuerza Sorprendente',
       description: 'Fuerza y control de grasa para todo tipo de cabellos',
       price: '700',
-      image: './images/cremas/Força Surpreendente.jpg'
+      image: './images/cremas/Força Surpreendente.jpg',
+      category: 'especials',
+      onfire: false
     }, {
       id: 16,
       title: 'Aceite de Argon',
       description: 'Nutrición y revitalización para cabellos secos y opacos',
       price: '700',
-      image: './images/cremas/Óleo de Argon.jpg'
+      image: './images/cremas/Óleo de Argon.jpg',
+      category: 'naturals',
+      onfire: false
     }, {
       id: 17,
       title: 'Aceite de Coco',
       description: 'Nutrición y revitalización profuda para cabellos secos y dañados',
       price: '700',
-      image: './images/cremas/Óleo de Coco.jpg'
+      image: './images/cremas/Óleo de Coco.jpg',
+      category: 'naturals',
+      onfire: false
     }, {
       id: 18,
       title: 'Vinagre de Manzana',
       description: 'Brillo y reducción de porosidad para cabellos resecos y con frizz',
       price: '700',
-      image: './images/cremas/Vinagre de Maçã.jpg'
+      image: './images/cremas/Vinagre de Maçã.jpg',
+      category: 'naturals',
+      onfire: false
     }, {
       id: 19,
       title: 'Arcilla Blanca',
       description: 'Brillo para cabellos secos y tratados químicamente',
       price: '700',
-      image: './images/cremas/Arguila Branca.jpg'
+      image: './images/cremas/Arguila Branca.jpg',
+      category: 'naturals',
+      onfire: false
     }, {
       id: 20,
       title: 'Mantega de Karité',
       description: 'Suavidad, sedosidad y brillo para cabellos resecos y fragiles',
       price: '700',
-      image: './images/cremas/Manteiga de Karité.jpg'
+      image: './images/cremas/Manteiga de Karité.jpg',
+      category: 'naturals',
+      onfire: false
     }, {
       id: 21,
       title: 'Cóctel Brasil',
       description: 'Nutrición y brillo con frutos del Amazonas',
       price: '700',
-      image: './images/cremas/Coquetel Brasil.jpg'
+      image: './images/cremas/Coquetel Brasil.jpg',
+      category: 'brasil',
+      onfire: false
     }, {
       id: 22,
       title: 'Maracujá',
       description: 'Crecimiento y fuerza a base maracujá y aceites de patauá',
       price: '700',
-      image: './images/cremas/Brasil - Maracujá.jpg'
+      image: './images/cremas/Brasil - Maracujá.jpg',
+      category: 'brasil',
+      onfire: false
     }, {
       id: 23,
       title: 'Mango',
       description: 'Nutrición y brillo a base mango y castañas del Pará',
       price: '700',
-      image: './images/cremas/Brasil - Manga.jpg'
+      image: './images/cremas/Brasil - Manga.jpg',
+      category: 'brasil',
+      onfire: false
     }, {
       id: 24,
       title: 'Caju',
       description: 'Resistencia y brillo a base de caju y murumuru',
       price: '700',
-      image: './images/cremas/Brasil - Caju.jpg'
+      image: './images/cremas/Brasil - Caju.jpg',
+      category: 'brasil',
+      onfire: false
     }, {
       id: 25,
       title: 'Café Verde',
       description: 'Vitalidad y brillo a base de café verde y aceite de Ucuuba',
       price: '700',
-      image: './images/cremas/Brasil - Café Verde.jpg'
+      image: './images/cremas/Brasil - Café Verde.jpg',
+      category: 'brasil',
+      onfire: false
     }, {
       id: 26,
       title: 'Banana',
       description: 'Reparación y brillo a base de banana y bacuri',
       price: '700',
-      image: './images/cremas/Brasil - Banana.jpg'
+      image: './images/cremas/Brasil - Banana.jpg',
+      category: 'brasil',
+      onfire: false
     }
   ]
 }
